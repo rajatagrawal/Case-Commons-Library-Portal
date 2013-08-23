aa985608091af24172ff80ea4c37d9aac6de13f7
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2013-08-22 22:40:27 -0400

    Change the data types of fields in user and book model

---
+++ db/migrate/20130821214800_change_column_types_books_users.rb
@@ -0,0 +1,21 @@
+class ChangeColumnTypesBooksUsers < ActiveRecord::Migration
+  def up
+    change_column :books, :title, :string
+    change_column :books, :author,  :string
+    change_column :books, :publisher, :string
+
+    change_column :users, :first_name, :string
+    change_column :users, :last_name, :string
+    change_column :users, :email_address, :string
+  end
+
+  def down
+    change_column :books, :title, :text
+    change_column :books, :author,  :text
+    change_column :books, :publisher, :text
+
+    change_column :users, :first_name, :text
+    change_column :users, :last_name, :text
+    change_column :users, :email_address, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130817200800) do
+ActiveRecord::Schema.define(:version => 20130821214800) do
 
   create_table "books", :force => true do |t|
-    t.text     "title"
-    t.text     "author"
-    t.text     "publisher"
+    t.string   "title"
+    t.string   "author"
+    t.string   "publisher"
     t.decimal  "price"
     t.integer  "user_id"
     t.datetime "created_at", :null => false
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(:version => 20130817200800) do
   end
 
   create_table "users", :force => true do |t|
-    t.text     "first_name"
-    t.text     "last_name"
-    t.text     "email_address"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email_address"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
   end
