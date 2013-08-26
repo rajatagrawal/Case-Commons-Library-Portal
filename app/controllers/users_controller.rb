8911c6ea5341b74e6aa066a57cce71b05307bca9
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2013-08-25 21:26:48 -0400

    Added users controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,19 @@ class UsersController< ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def destroy
+    @user = User.find(params[:id]).destroy
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.create(params[:user])
+  end
 end
