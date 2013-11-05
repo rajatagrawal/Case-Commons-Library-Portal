d9c792cf745b5f835e21bf5ebf731f4740a99ad5
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2013-11-04 23:32:57 -0500

    Added login feature specs

---
+++ spec/features/login.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+feature 'login page', js: true do
+  fixtures :users
+
+  scenario 'user logs in successfully' do
+    visit new_user_session_path
+    fill_in 'Email', with: users(:rajat).email
+    fill_in 'Password', with: 'Password123'
+    click_button('Sign in')
+    expect(page).to have_content('Signed in successfully.')
+  end
+
+  scenario 'user logs in unsuccessfully' do
+    visit new_user_session_path
+    fill_in 'Email', with: users(:rajat).email
+    fill_in 'Password', with: 'wrong_password'
+    click_button('Sign in')
+    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content('Invalid email or password.')
+  end
+end
