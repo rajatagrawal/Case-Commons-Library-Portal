3c2e0c3bda42cd8000f69e8fa9573f3551690fde
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2013-11-30 09:45:57 -0500

    Modified the book new page

---
+++ spec/views/books/new.html.haml_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'books/new' do
+  let(:page) { Capybara.string(render) }
+
+  let(:book) { Book.new }
+
+  before do
+    assign(:book, book)
+  end
+
+  it 'shows the labels for the fields' do
+    expect(page).to have_content('Enter the Title of the book')
+    expect(page).to have_content('Enter the Author of the book')
+    expect(page).to have_content('Enter the Publisher of the book')
+    expect(page).to have_content('Enter the Price of the book')
+  end
+
+  it 'has an add the book button' do
+    expect(page).to have_button('Add the book')
+  end
+
+  it 'displays the heading of the page' do
+    expect(page).to have_css('h2','Add a Book')
+  end
+end
