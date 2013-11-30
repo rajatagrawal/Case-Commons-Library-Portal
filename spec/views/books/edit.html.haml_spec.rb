479024f864770fc9f31e5bdd5c9aa1a2375e1518
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2013-11-30 14:54:02 -0500

    Modified the book edit page

---
+++ spec/views/books/edit.html.haml_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'books/edit' do
+
+  fixtures :all
+
+  let(:page) { Capybara.string(render) }
+  before do
+    allow(view).to receive(:current_user).and_return(users(:admin))
+    assign(:book, Book.first)
+  end
+
+  it 'shows the header' do
+    expect(page).to have_css('h2', 'Edit the book')
+  end
+
+  it 'shows the labels for the input fields' do
+    expect(page).to have_content('Enter the title for the book')
+    expect(page).to have_content('Enter the author for the book')
+    expect(page).to have_content('Enter the publisher for the book')
+    expect(page).to have_content('Enter the price for the book')
+  end
+
+end
