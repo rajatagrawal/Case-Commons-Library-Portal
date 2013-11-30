ee0d82e7431650d3196105a3d68b38a57433a6d3
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2013-11-30 13:39:07 -0500

    Add delete button for admin user on book show page

--- spec/views/books/show.html.haml_spec.rb
+++ spec/views/books/show.html.haml_spec.rb
@@ -4,9 +4,21 @@ describe 'books/show' do
   fixtures :all
   let(:page) { Capybara.string(render) }
 
+  context 'when the logged in user is admin' do
+    before do
+      assign(:book, books(:book1))
+      allow(view).to receive(:current_user).and_return(users(:admin))
+    end
+
+    it 'shows a delete button' do
+      expect(page).to have_button('Delete Book')
+    end
+  end
+
   context 'when the book is checked out' do
     let(:book) { books(:issued_book) }
     before do
+      allow(view).to receive(:current_user).and_return(users(:employee))
       assign(:book, book)
     end
 
@@ -18,7 +30,6 @@ describe 'books/show' do
 
     it 'shows a check in button' do
       expect(page).to have_button('Check In')
-
     end
 
   end
@@ -26,6 +37,7 @@ describe 'books/show' do
   context 'when the book is not checked out' do
     let(:book) { books(:unissued_book) }
     before do
+      allow(view).to receive(:current_user).and_return(users(:employee))
       assign(:book, book)
     end
 
@@ -37,6 +49,5 @@ describe 'books/show' do
     it 'shows a check in button' do
       expect(page).to have_button('Check Out')
     end
-
   end
 end
