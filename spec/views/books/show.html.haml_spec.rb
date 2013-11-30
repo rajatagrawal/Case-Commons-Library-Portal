d9d84c9c325d41f46cfe80a91e3cda22bfb0b77b
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2013-11-30 00:30:30 -0500

    Add view spec for the check in feature

--- spec/views/books/show.html.haml_spec.rb
+++ spec/views/books/show.html.haml_spec.rb
@@ -1,20 +1,42 @@
 require 'spec_helper'
 
 describe 'books/show' do
-  let(:book) do
-    stub_model Book, title: 'Book Title'
-    stub_model Book, author: 'Book Author'
-    stub_model Book, publisher: 'Book Publisher'
-    stub_model Book, price: 10
-  end
+  fixtures :all
   let(:page) { Capybara.string(render) }
 
-  before(:each) do
-    assign :book, book
+  context 'when the book is checked out' do
+    let(:book) { books(:issued_book) }
+    before do
+      assign(:book, book)
+    end
+
+    it 'shows who the book is issued to ' do
+      expect(page).to have_content('Issued by')
+      expect(page).to have_content(book.user.first_name + book.user.last_name)
+
+    end
+
+    it 'shows a check in button' do
+      expect(page).to have_button('Check In')
+
+    end
+
   end
 
-  it 'shows a checkout button for the book' do
-    binding.pry
-    expect(page.has_button?('Checkout')).to be_true
+  context 'when the book is not checked out' do
+    let(:book) { books(:unissued_book) }
+    before do
+      assign(:book, book)
+    end
+
+    it 'shows no body has issued the book' do
+      expect(page).to have_content('Issued by')
+      expect(page).to have_content('None')
+    end
+
+    it 'shows a check in button' do
+      expect(page).to have_button('Check Out')
+    end
+
   end
 end
