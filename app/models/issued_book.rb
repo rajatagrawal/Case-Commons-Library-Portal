4a0ef7c1b38812a7386fdcc65a233a06552db5e2
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2013-12-09 23:00:25 -0500

    Add IssuedBook join model

---
+++ app/models/issued_book.rb
@@ -0,0 +1,8 @@
+class IssuedBook < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :book
+
+  validates :user, presence: true
+  validates :book, presence: true
+  validates :issued_on, presence: true
+end
---
+++ spec/models/issued_book_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe IssuedBook do
+  it 'belongs to a user' do
+    expect(subject).to belong_to :user
+  end
+
+  it 'belongs to a book' do
+    expect(subject).to belong_to :book
+  end
+
+  it 'should validate presense of the user' do
+    expect(subject).to validate_presence_of :user
+  end
+
+  it 'should validate presense of the user' do
+    expect(subject).to validate_presence_of :book
+  end
+
+  it 'should validate presense of the user' do
+    expect(subject).to validate_presence_of :issued_on
+  end
+end
